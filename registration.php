3c1a10ae8ebdf6e0db0e14c5803088d53f3487c1
Festus murimi <sinotechxvista@gmail.com>
2019-11-14 16:06:57 +0300

    Add files via upload

--- registration.php
+++ registration.php
@@ -1,3 +1,6 @@
+
+<!--========================= END OF PHP SCRIPT =====================----------->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +8,16 @@
 <!--====================== META DATA ===========================-->
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="keywords" content="university bookstore, book shraing, free students notes, free notes downlaod">
-	<meta name="description" content="university book store offer free platform to allows students from different universities and courses share important documents and notes online and provide free downloads">
+	<meta name="keywords" content="kabarak university book-store, book shraing, free students notes, free notes downlaod">
+	<meta name="description" content="kabarak university book store offer free platform to allows students from different courses share important documents and notes online and provide free downloads">
 	<meta name="author-name" content="festus murimi">	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 	<!--==================== STYLE SHEETS ==================================-->
 
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">	
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/login-registration.css">
 </head>
@@ -21,10 +25,10 @@
 	<div class="limiter">
 		<div class="container-login" style="background-image: url('images/Essential-Books.jpg');">
 			<div class="wrap-login p-l-50 p-r-50 p-t-72 p-b-50">
-				<form class="login-form validate-form" action="registration.php" method="post">
-				<?php if(!empty($message)): ?>
-					<p class="txt1"><?= $message ?></p>
-				<?php endif; ?>
+			<?php require "php/signup2.php" ?>
+				<form class="login-form validate-form" action="php/signup2.php" method="post">
+					<p class="txt1"><?= $errors ?></p>
+
 					<span class="login-form-title p-b-59">
 						Sign Up
 					</span>
@@ -38,12 +42,6 @@
 						<input class="input" type="text" name="email" placeholder="Email addess">
 						<span class="focus-input" data-placeholder="&#9993;"></span>
 					</div>
-<!--================================= ADD UNIVERSITY SECTION ===============================================---->
-
-
-
-
-
 <!--================================ PASSWORD INPUT SECTION    =============================================-->
 					<div class="wrap-input validate-input" data-validate = "Password is required">
 						<input class="input" type="password" name="password" placeholder="Password">
@@ -51,7 +49,7 @@
 					</div>
 <!--================================ REPEAT PASSWORD INPUT SECTION    =============================================-->
 					<div class="wrap-input validate-input" data-validate = "Repeat Password is required">
-						<input class="input" type="password" name="repeat_password" placeholder="confirm_Password">
+						<input class="input" type="password" name="confirm_password" placeholder="confirm_Password">
 						<span class="focus-input"  data-placeholder="&#xf191;"></span>
 					</div>
 <!--================================ TERMS AND CONDITION SECTION    =============================================-->
@@ -86,6 +84,5 @@
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 	<script src="js/main.js"></script>
-
 </body>
 </html>
